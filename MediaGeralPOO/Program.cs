d9ba0cb33fa799a5ff7fed07998b43b13cee66fb
Ryszard Mubarak <ryszard696@yahoo.com.br>
2022-01-27 19:59:57 -0300

    finalizado

--- MediaGeralPOO/Program.cs
+++ MediaGeralPOO/Program.cs
@@ -1,9 +1,52 @@
-﻿namespace MediaGeralPOO
+﻿using System;
+
+namespace MediaGeralPOO
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.Title = "### MÉDIA GERAL DOS ALUNOS ###";
+
+            Console.WriteLine("Quantos Alunos para gerar a média? ");
+            int nAlunos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Aluno[] alunos = new Aluno[nAlunos];
+
+            for (int i = 0; i < alunos.Length; i++)
+            {
+                Console.Clear();
+                Console.Write($"Aluno # {i + 1} Nome ..: ");
+                string nome = Console.ReadLine();
+
+                Console.Write($"Aluno # {i + 1} Provas.: ");
+                int provas = int.Parse(Console.ReadLine());
+
+                alunos[i] = new Aluno(nome, provas);
+                Console.WriteLine($"Insira as notas do Aluno {nome}");
+                alunos[i].InserirNotas();
+            }
+
+            Console.Clear();
+
+            double mediaGeral = 0;
+
+            foreach (Aluno aluno in alunos)
+            {
+                Console.WriteLine($"Aluno: {aluno.Nome}");
+                Console.WriteLine($"Média: {aluno.Media}");
+                Console.WriteLine();
+
+                mediaGeral += aluno.Media;
+            }
+
+            double resultadoFinal = mediaGeral / alunos.Length;
+
+            Console.WriteLine($"Média geral dos alunos : {resultadoFinal}");
+
+            Console.ReadKey();
         }
     }
 }
